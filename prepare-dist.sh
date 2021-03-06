497a9d787a8c199a72f7c4c7ad94bc8f957de1df
trusko <tom.rusko@asideas.de>
2021-03-06 22:43:28 +0100

    fixes typo

--- prepare-dist.sh
+++ prepare-dist.sh
@@ -2,4 +2,4 @@ tsc --declarationDir ./dist --outDir ./dist
 cp package.json dist
 cp -r icons dist
 cp src/diagram/aws/awsResouceIconMatches.json dist/src/diagram/aws/awsResouceIconMatches.json
-cd README.md dist
+cp README.md dist
