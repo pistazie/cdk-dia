55da3a5a3423ad95a1d306d6233b18724ec5f801
Tom <hi@pistazie.rocks>
2021-10-02 20:29:36 +0200

    Improves smoke test using Jest. provokes failure for testing

---
+++ .github/smoke-test/smoke-test.sh
@@ -0,0 +1,42 @@
+# Publish to local npm registry
+npm install -g verdaccio
+verdaccio --config smoke-test-npm-local-repo-config.yml &
+npm run prepare-dist
+npm version 9.9.${GITHUB_RUN_NUMBER} --commit-hooks false --git-tag-version false
+npm publish --registry http://localhost:4873
+
+# Use in a CDK project
+npm install -g typescript
+npm install -g aws-cdk
+sudo apt install graphviz
+mkdir package-smoke-test
+cd package-smoke-test
+cdk init sample-app --language typescript
+npm install cdk-dia@9.9.${GITHUB_RUN_NUMBER} --registry http://localhost:4873
+
+## Use CLI and test expected PNG
+cdk synth
+./node_modules/.bin/cdk-dia
+ls -la
+SMOKE_TEST_BASE_PATH=$(pwd)
+cd ./.github/smoke-test
+npm i
+npm run test
+
+# Use as a globally installed package
+npm install -g typescript
+npm install -g aws-cdk
+
+sudo apt install graphviz
+mkdir package-smoke-test-global
+cd package-smoke-test-global
+cdk init sample-app --language typescript
+
+# Use CLI and test expected PNG
+cdk synth
+cdk-dia
+ls -la
+SMOKE_TEST_BASE_PATH=$(pwd)
+cd ./.github/smoke-test
+npm i
+npm run test
